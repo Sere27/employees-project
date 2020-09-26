e742e372fa3e9c7ed2cdb2dbea38c9591033f1c2
Sere27 <vetbat1126@gmail.com>
2020-09-26 18:32:54 +0300

    employee update function added

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const form = document.getElementById("employee-form");
 const nameInput = document.getElementById("name");
 const departmentInput = document.getElementById("department");
 const salaryInput = document.getElementById("salary");
-const employeesList2 = document.querySelector("#employees");
+const employeesList = document.querySelector("#employees");
 const updateButton = document.getElementById("update");
 
 const request = new Request("http://localhost:3000/employees");
 
 const ui = new UI();
+
+let updateState = null;
 
 // request
 //     .get()
@@ -45,6 +47,9 @@ eventListeners();
 
 function eventListeners() {
 	document.addEventListener("DOMContentLoaded", getAllEmployees);
+	form.addEventListener("submit", addEmployee);
+	employeesList.addEventListener("click", updateOrDelete);
+	updateButton.addEventListener("click", updateEmployee);
 }
 
 function getAllEmployees() {
@@ -55,4 +60,86 @@ function getAllEmployees() {
 			ui.addAllEmployeesToUI(employees);
 		})
 		.catch((err) => console.log("err >> ", err));
+}
+
+function addEmployee(e) {
+	const employeeName = nameInput.value.trim();
+	const employeeDepartment = departmentInput.value.trim();
+	const employeeSalary = salaryInput.value.trim();
+	if (
+		employeeName === "" ||
+		employeeDepartment === "" ||
+		employeeSalary === ""
+	) {
+		alert("Tüm alanları doldurunuz!!");
+	} else {
+		request
+			.post({
+				name: employeeName,
+				department: employeeDepartment,
+				salary: Number(employeeSalary),
+			})
+			.then((employee) => {
+				ui.addEmployeeToUI(employee);
+			})
+			.catch((err) => console.log(err));
+	}
+	ui.clearInputs();
+	e.preventDefault();
+}
+
+function updateOrDelete(e) {
+	if (e.target.id === "delete-employee") {
+		//silme
+		deleteEmployee(e.target);
+	} else if (e.target.id === "update-employee") {
+		//güncelleme
+		//tr yi aldık
+		updateEmployeeController(e.target.parentElement.parentElement);
+	}
+}
+
+function deleteEmployee(targetEmployee) {
+	//idyi bulduk
+	const id =
+		targetEmployee.parentElement.previousElementSibling.previousElementSibling
+			.textContent;
+
+	request
+		.delete(id)
+		.then((message) => {
+			ui.deleteEmployeeFromUI(targetEmployee.parentElement.parentElement);
+		})
+		.catch((err) => console.log("err >> ", err));
+}
+
+function updateEmployeeController(targetEmployee) {
+	ui.toggleUpdateButton(targetEmployee);
+
+	if (updateState === null) {
+		updateState = {
+			updateId: targetEmployee.children[3].textContent,
+			updateParent: targetEmployee,
+		};
+	} else {
+		updateState = null;
+	}
+}
+
+function updateEmployee() {
+	if (updateState) {
+		//null değilse
+		const data = {
+			name: nameInput.value.trim(),
+			department: departmentInput.value.trim(),
+			salary: Number(salaryInput.value.trim()),
+		};
+
+		request
+			.put(updateState.updateId, data)
+			.then((updatedEmployee) => {
+				ui.updateEmployeeOnUI(updatedEmployee, updateState.updateParent);
+			})
+			.catch((err) => console.log("errr >> ", errr));
+	}
 }
